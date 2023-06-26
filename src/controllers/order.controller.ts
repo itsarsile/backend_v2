453d51174affd280322259d180afe696d329a661
Arsile <ajiprio@hotmail.com>
2023-06-26 10:03:05 +0700

    Update routes and services

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -2,9 +2,9 @@ import { NextFunction, Request, Response } from "express";
 import {
   createOrder,
   deleteOrder,
+  getOrderById,
   getOrders,
 } from "../services/order.service";
-import AppError from "../utils/appError";
 
 export const createOrderHandler = async (
   req: Request,
@@ -15,7 +15,6 @@ export const createOrderHandler = async (
     const userId = res.locals.user.id;
     const addressId = req.query.addressId as string;
     const { orderItems } = req.body;
-    console.log(orderItems);
     const order = await createOrder({ userId, addressId, orderItems });
 
     res.status(201).json({
@@ -47,6 +46,56 @@ export const getOrderHandler = async (
   }
 };
 
+export const getOrderByIdHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const orderId = req.params.id;
+    const order = await getOrderById(orderId);
+
+    if (!order) {
+      return res.status(404).json({
+        status: "error",
+        message: "Order not found",
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        order,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+// export const updateOrderItemsHandler = async (
+//   req: Request,
+//   res: Response,
+//   next: NextFunction,
+// ) => {
+//   try {
+//     const orderId = req.params.id;
+//     const { orderItems } = req.body;
+    
+//     const updatedOrder = await updateOrderItems(orderId, orderItems);
+
+//     res.status(200).json({
+//       status: "success",
+//       data: {
+//         order: updatedOrder,
+//       },
+//     });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
 export const deleteOrderHandler = async (
   req: Request,
   res: Response,
@@ -64,5 +113,3 @@ export const deleteOrderHandler = async (
     next(error);
   }
 };
-
-
